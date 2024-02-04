2d44c4e720dc6c558fe7b0a0df3060e55beb566b
ManasiNarkhede <132153639+ManasiNarkhede@users.noreply.github.com>
2024-02-05 00:12:47 +0530

    Update DiagnosisTable.sql

--- Hospital Management/DiagnosisTable.sql
+++ Hospital Management/DiagnosisTable.sql
@@ -15,5 +15,13 @@ SELECT DISTINCT PatientID FROM patients;
 
 -- Query to add a diagnosis for a patient
 INSERT INTO Diagnosis (DiagnosisID, PatientID, DiagnosisDetails, DateTagged)
-VALUES (1, 1, 'Cold', '2020-02-01');
+VALUES (1, 1, 'Cold', '2020-02-01'),
+	(2,2,'Fever','2023-06-09'), 
+	(3, 3, 'Obesity', '2021-04-23'), 
+	(4, 4, 'Diabities', '2021-10-03'), 
+	(5, 5, 'Cancer', '2022-02-17');
+
+-- Display data from the Diagnosis table
+SELECT * FROM Diagnosis;
+
 
