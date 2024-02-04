9693d66a41130a1e86b783e3d2db7e105733e561
ManasiNarkhede <132153639+ManasiNarkhede@users.noreply.github.com>
2024-02-05 00:10:46 +0530

    Update AppointmentTable.sql

--- Hospital Management/AppointmentTable.sql
+++ Hospital Management/AppointmentTable.sql
@@ -10,3 +10,16 @@ CREATE TABLE Appointments (
     FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
     FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
 );
+
+-- Inserting data into the Appointments table
+INSERT INTO Appointments (AppointmentID, PatientID, DoctorID, AppointmentDate, Status)
+VALUES
+    (1, 1, 101, '2024-01-05', 'Completed'),
+    (2, 2, 102, '2024-04-06', 'Scheduled'),
+    (3, 3, 103, '2024-03-07', 'Cancelled'),
+    (4, 4, 104, '2023-02-08', 'Completed'),
+    (5, 5, 105, '2024-02-19', 'Scheduled');
+    
+-- Display data from the Appointment table
+SELECT * FROM Appointments;
+
