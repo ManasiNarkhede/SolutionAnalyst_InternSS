164568fbea22c0dcef11b73df66bdea15dfd7670
ManasiNarkhede <132153639+ManasiNarkhede@users.noreply.github.com>
2024-02-05 00:51:41 +0530

    Update MedicalRecordTable.sql

--- Hospital Management/MedicalRecordTable.sql
+++ Hospital Management/MedicalRecordTable.sql
@@ -13,3 +13,16 @@ CREATE TABLE MedicalRecords (
     FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID),
     FOREIGN KEY (AppointmentID) REFERENCES Appointments(AppointmentID)
 );
+
+
+-- Insert data into MedicalRecords
+INSERT INTO MedicalRecords (RecordID, PatientID, DoctorID, AppointmentID, Diagnosis, Prescription, DateRecorded)
+VALUES
+  (201, 1, 101, 1, 'Common Cold', 'Ibuprofen and rest', '2024-01-05'),
+  (202, 2, 102, 2, 'Fever', 'Ibuprofen and rest', '2024-04-06'),
+  (203, 3, 103, 3, 'Obesity', 'Daily exercise', '2024-03-07'),
+  (204, 4, 104, 4, 'Diabities', 'Insulin', '2023-02-08'),
+  (205, 5, 105, 5, 'Cancer', 'Chemotherapy',  '2024-02-19');
+
+-- Display data from the Medical Record table
+SELECT * FROM MedicalRecords;
