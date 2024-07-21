c12f5aac38957dd8342a342bf853200ce8d9258c
Manasi <narkhedemanasi28@gmail.com>
2024-07-21 21:47:58 +0530

    commit

---
+++ Month5_6/Cake shop/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('order-now').addEventListener('click', () => {
+    alert('Ordering functionality will be available soon!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
